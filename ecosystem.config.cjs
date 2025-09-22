5b84b44b1bd9255a2717f1d444b93931db83e406
Taurai Job Munodawafa <taujob1111@gmail.com>
2025-09-22 15:30:43 +0200

    port

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,7 +2,7 @@ module.exports = {
     apps: [
         {
             name: 'Webdev Resources Booking',
-            port: '3055',
+            port: '3006',
             exec_mode: 'cluster',
             instances: '1',
             script: './.output/server/index.mjs'
