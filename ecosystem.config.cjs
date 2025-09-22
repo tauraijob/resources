8adaf2cefeb1af77d30a0e3433b10589db07afed
Taurai Job Munodawafa <taujob1111@gmail.com>
2025-09-22 15:45:52 +0200

    Update application port from 3006 to 3012 in ecosystem configuration

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,7 +2,7 @@ module.exports = {
     apps: [
         {
             name: 'Webdev Resources Booking',
-            port: '3006',
+            port: '3010',
             exec_mode: 'cluster',
             instances: '1',
             script: './.output/server/index.mjs'
